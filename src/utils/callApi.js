1a7183a6bd1dd834d5329c948ecf2413e0b2da73
Glitchyi <mediaclub@mgits.ac.in>
2023-03-28 15:44:39 +0530

    chenges

--- src/utils/callApi.js
+++ src/utils/callApi.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 export default function callApi(endpoint, data) {
     return new Promise((resolve, reject) => {
-        const env = 'DEV'
+        const env = 'PRODUCTION'
         const BASE_URL = env === 'PRODUCTION' ? 'https://gps-cidre-352715.el.r.appspot.com' : 'http://localhost:3000'
         const url = `${BASE_URL}/api/${endpoint}`
         const config = { headers: { "Authorization": `Bearer ${localStorage.getItem('user_token') && JSON.parse(localStorage.getItem('user_token')).value}` } }
